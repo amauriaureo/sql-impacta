3e5e7497fa80a96a7cc6c6ee93c415530355b795
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-22 07:56:26 -0300

    estoqueProduto

---
+++ modelo1/tabelas/estoque_produto.py
@@ -0,0 +1,16 @@
+import sqlite3
+connection = sqlite3.connect('todo.sqlite3')
+
+cursor = connection.cursor()
+
+sql = '''
+create table EstoqueProduto (
+    id INTENGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    produtoId VARCHAR(20),
+        FOREIGN KEY (produtoId) REFERENCES produto(id)
+)
+'''
+
+cursor.execute(sql)
+connection.commit()
+connection.close()
